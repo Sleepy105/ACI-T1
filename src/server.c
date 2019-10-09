a7a6a5c537555285daecca2de535f0d7d8525dfa
Sleepy105 <lm.sousa@fe.up.pt>
2019-10-09 11:51:37 +0100

    Add server functionality

--- src/server.c
+++ src/server.c
@@ -1,5 +1,46 @@
 #include "server.h"
 
+
+int getServerCoil (Server* server, size_t n) {
+    if (!server) {
+        return SOCK_VALUE_OUT_OF_BOUNDS;
+    }
+
+    if (n < 0 || n > N_COILS) {
+        return SOCK_VALUE_OUT_OF_BOUNDS;
+    }
+
+    size_t holder_pos = n/8;
+    uint8_t pos_in_holder = n%8;
+    uint8_t pos_mask = 0x01 << pos_in_holder;
+
+    return (server->coils[holder_pos] & pos_mask) ? 1 : 0;
+}
+
+int setServerCoil (Server* server, size_t n, bool value) {
+    if (!server) {
+        return SOCK_VALUE_OUT_OF_BOUNDS;
+    }
+
+    if (n < 0 || n > N_COILS) {
+        return SOCK_VALUE_OUT_OF_BOUNDS;
+    }
+
+    size_t holder_pos = n/8;
+    uint8_t pos_in_holder = n%8;
+    uint8_t pos_mask = 0x01 << pos_in_holder;
+    
+    if (value) {
+        server->coils[holder_pos] |= pos_mask;
+    }
+    else {
+        server->coils[holder_pos] &= ~pos_mask;
+    }
+
+    return SOCK_OK;
+}
+
+
 int serverInit (Server* server, uint16_t port) {
     server->socket = socketCreate();
     if (server->socket < 0) {
--- src/server.h
+++ src/server.h
@@ -1,16 +1,25 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__
 
+#include <stdbool.h>
 #include <socket.h>
 
 #define SERVER_ADDRESS "127.0.0.1"
+#define N_REGS 20
+#define N_COILS 20
 
 typedef struct {
     int socket;
     uint16_t port;
+    uint16_t h_regs[N_REGS];
+    uint8_t coils[N_COILS];
     char* buffer_out;
     char* buffer_in;
 } Server;
+
+int getServerCoil (Server* server, size_t n);
+
+int setServerCoil (Server* server, size_t n, bool value);
 
 int serverInit (Server* server, uint16_t port);
 
