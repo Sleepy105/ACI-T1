714b748e8a942f497d45b68d9f4122d895c36906
Sleepy105 <lm.sousa@fe.up.pt>
2019-10-10 13:18:00 +0100

    Add missing prototype

--- lib/ModbusTCP/ModbusTCP.c
+++ lib/ModbusTCP/ModbusTCP.c
@@ -60,6 +60,9 @@ int Send_Modbus_request(char* server_add, uint16_t port, uint8_t* apdu, size_t a
     return 0;
 }
 
+int Receive_Modbus_response(int fd, uint8_t* apdu , size_t apdu_len) {
+    return 0;
+}
 
 int Send_Modbus_response(int TI, uint8_t* resp_apdu , size_t resp_apdu_size) {
     return 0;
--- lib/ModbusTCP/ModbusTCP.h
+++ lib/ModbusTCP/ModbusTCP.h
@@ -10,6 +10,7 @@
 
 int Send_Modbus_request(char* server_add, uint16_t port, uint8_t* apdu, size_t apdu_size, uint8_t* r_apdu);
 
+int Receive_Modbus_response(int fd, uint8_t* apdu , size_t apdu_len);
 
 int Send_Modbus_response(int TI, uint8_t* resp_apdu , size_t resp_apdu_size);
 
