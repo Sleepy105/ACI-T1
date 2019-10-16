2b19b5194b317e1cfbe63023bde47daf5a2df546
Sleepy105 <lm.sousa@fe.up.pt>
2019-10-16 09:33:14 +0100

    Instantiate several clients for testing

--- src/main.c
+++ src/main.c
@@ -4,66 +4,52 @@
 
 #include "socket.h"
 #include "server.h"
+#include "client.h"
 
 #define BUFFER_LENGTH 32
 
 Server server;
+
+Client client1;
+Client client2;
 
 /* bool test_1 (void* server, void* client) {
     int res = client.write_multiple_regs(server);
     
 }*/
 
-int main(int argc, char const *argv[])
-{
+// int main(int argc, char const *argv[]) {
+int main() {
     if (serverInit(&server, 5502, 10)) {
         printf("Error starting the server\n");
         return -1;
     }
+    else {
+        printf("Server 1 started\n");
+    }
     pthread_t thread;
     pthread_create(&thread, NULL, &thread_serverLoop, &server);
 
-    int socket = socketCreate();
-    if (socket < 0) {
-        printf("Error creating the client's socket.\n");
-        return -1;
+    if (clientInit(&client1, "1", "127.0.0.1", 5502)) {
+        printf("ERROR creating client 1\n");
     }
-    if (socketConnect(socket, "127.0.0.1", 5502) < 0) {
-        printf("Error connecting to the server.\n");
-        socketClose(socket);
-        return -1;
+    else {
+        printf("Client 1 started\n");
+    }
+    if (clientInit(&client2, "2", "127.0.0.1", 5502)) {
+        printf("ERROR creating client 2\n");
+    }
+    else {
+        printf("Client 2 started\n");
     }
 
-    char buf[BUFFER_LENGTH] = {0};
     while (1) {
-        scanf("%s", buf);
-        if (strnlen(buf, BUFFER_LENGTH) == 1 && buf[0] == '\\') {
-            // Close the connection if a single '\' is sent
-            break;
-        }
-
-        // Send the buffer contents
-        int r = socketWrite(socket, buf, strnlen(buf, BUFFER_LENGTH));
-        if (r < 0) {
-            printf("Error sending: %d\n", r);
-        }
-        else {
-            printf("SENT     %d bytes: %s\n", r, buf);
-        }
-
-        // Clear the buffer
-        for (int i = 0; i < BUFFER_LENGTH; i++) {
-            buf[i] = 0;
-        }
-
-        r = socketRead(socket, buf, BUFFER_LENGTH);
-        if (!r) {
-            printf("Error Receiving (client)\n");
-        }
-        printf("Received from server: %s\n", buf);
+        clientProcess(&client1);
+        clientProcess(&client2);
     }
 
-    socketClose(socket);
+    clientClose(&client1);
+    clientClose(&client2);
     printf("Connection Closed\n");
 
     return 0;
