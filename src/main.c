a9c1cc63688b368d1bede52c456fde2920718169
Sleepy105 <lm.sousa@fe.up.pt>
2019-10-16 12:15:58 +0100

    Dynamically allocate clients

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "socket.h"
 #include "server.h"
@@ -24,25 +25,30 @@ int main() {
         return -1;
     }
     else {
-        printf("Server 1 started\n");
+        printf("Server 0 started\n");
     }
     pthread_t thread;
     pthread_create(&thread, NULL, &thread_serverLoop, &server);
 
     // INIT CLIENTS
     for (int i = 0; i < NUMBER_OF_CLIENTS; i++) {
+        size_t num_of_chars = i ? (ceil(log10(i))+1) : 1;
+        char* str = malloc((num_of_chars+1)*sizeof(char));
+        snprintf(str, num_of_chars+1, "%d", i);
         if (clientInit(&client[i], "1", SERVER_ADDRESS_FOR_CLIENT_CONNECTION, SERVER_PORT_FOR_CLIENT_CONNECTION)) {
-            printf("ERROR creating client 1\n");
+            printf("ERROR creating client %s\n", str);
         }
         else {
-            printf("Client 1 started\n");
+            printf("Client %s started\n", str);
         }
+        free(str);
     }
 
 
     // RUN TESTS
     while (1) {
         for (int i = 0; i < NUMBER_OF_CLIENTS; i++) {
+            getchar();
             clientReadRegs(&client[i], 0, 2);
         }
     }
